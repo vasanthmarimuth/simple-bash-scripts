18fffa2e3291bcc91a6d6412055a0b5fc098664f
Vasanth Marimuthu <105814254+vasanthmarimuth@users.noreply.github.com>
2024-09-30 11:04:16 +0530

    Add files via upload

---
+++ BashScripting/operations.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# basic arithmetic using Backticks and expr
+
+a=10
+b=20
+echo "a+b=` expr $a + $b ` "
+
+
+# basic arthimetic using Double Parentheses
+
+c=34
+d=4
+echo "c-d=$(( $c-$d))"
+
+
+# basic arthimetic using Let Construction
+
+
+e=20
+f=100
+let "ans=$(($e*$f))"
+echo "e*f=$ans"
